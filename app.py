528bf8a9e20d526cdcc0c266beaf16eed3ec32eb
SANJEEV SAH <104579917+Sanjeeviit@users.noreply.github.com>
2023-12-07 21:41:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-import streamlit as st
-st.title("Calculator")
-def addition(a+b):
-  return a+b
+#import streamlit as st
+#st.title("Calculator")
+#def addition(a+b):
+  #return a+b
   
-a = st.number_input('first number')
-b = st.number_input('second number')
+#a = st.number_input('first number')
+#b = st.number_input('second number')
+import streamlit as st
+
+st.title("Calculator")
+
+def addition(a, b):
+    return a + b
+
+a = st.number_input('Enter the first number')
+b = st.number_input('Enter the second number')
+
+result = addition(a, b)
+
+st.write(f"The result of addition is: {result}")
